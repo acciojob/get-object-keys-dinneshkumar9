d0bd4d791e1e4af71c94970b673f8bfb7b3bb287
Dinesh kumar <dinneshkumar9@gmail.com>
2023-03-21 19:53:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// Create student object with a name property
+const student = {
+name: "John Doe"
+};
+
+// Add getKeys() method to Object prototype
+Object.prototype.getKeys = function() {
+return Object.keys(this);
+};
+
+// Test getKeys() method on student object
+console.log(student.getKeys()); // Output: ["name"]
